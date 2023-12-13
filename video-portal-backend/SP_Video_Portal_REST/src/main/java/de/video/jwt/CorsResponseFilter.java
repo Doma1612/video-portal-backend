7966b19d774559a987e88ba8116406ea61c28083
Doma1612 <dominik.martens@outlook.com>
2023-12-13 16:15:54 +0100

    add requested changes

--- video-portal-backend/SP_Video_Portal_REST/src/main/java/de/video/jwt/CorsResponseFilter.java
+++ video-portal-backend/SP_Video_Portal_REST/src/main/java/de/video/jwt/CorsResponseFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -29,6 +30,10 @@ public class CorsResponseFilter implements Filter {
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         httpResponse.setHeader("Access-Control-Max-Age", "3600");
+        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) request).getMethod())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
 
         chain.doFilter(request, response);
     }
