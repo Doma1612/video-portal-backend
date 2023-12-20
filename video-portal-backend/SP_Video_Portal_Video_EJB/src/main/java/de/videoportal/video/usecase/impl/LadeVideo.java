2fde55665c07e46f02e3ed0e32fca06bae14c654
Doma1612 <dominik.martens@outlook.com>
2023-12-20 14:35:18 +0100

    fix metadata search

--- video-portal-backend/SP_Video_Portal_Video_EJB/src/main/java/de/videoportal/video/usecase/impl/LadeVideo.java
+++ video-portal-backend/SP_Video_Portal_Video_EJB/src/main/java/de/videoportal/video/usecase/impl/LadeVideo.java
@@ -46,7 +46,7 @@ public class LadeVideo implements ILadeVideo {
         List<VideoTO> videoList = new ArrayList<>();
         for (Video vid : allVids) {
             for (String stichwort : stichwoerterList) {
-                if (vid.getMetaData().contains(stichwort)) {
+                if (vid.getMetaData().contains(stichwort.toLowerCase())) {
                     VideoTO to = vid.toVideoTO();
                     to.setName(to.getName().replace("_", " "));
                     videoList.add(to);
